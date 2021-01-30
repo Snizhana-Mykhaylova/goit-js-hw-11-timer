42535e246ea45fc49721d7fcbed0961462f451db
Snizhana-Mykhaylova <snejana.pallada@gmail.com>
2021-01-30 21:49:27 +0200

    add timer.js

---
+++ src/js/timer.js
@@ -0,0 +1,49 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const targetTime = this.targetDate.getTime();
+      const deltaTime = targetTime - currentTime;
+
+      this.upDateTimerFace(deltaTime);
+    }, 1000);
+  }
+
+  upDateTimerFace(time) {
+    const days = this.padDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  padDays(value) {
+    return String(value).padStart(3, '0');
+  }
+}
+
+const timer = new CountdownTimer('#timer-1', new Date('Jul 17, 2021'));
+
+timer.start();
